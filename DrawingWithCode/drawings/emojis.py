fb3bcb6ca9519e08d037a8e13ad223ba1f77284e
Sarah Daigle <sarahannedaigle@gmail.com>
2017-04-03 16:08:53 -0500

    Adding in the first set of sample files

---
+++ DrawingWithCode/drawings/emojis.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+print("Welcome to Emoji Party")
+
+# Here's a Comment
+
+#setup a Surface / Canvas to work on
+
+
+pygame.init()
+pygame.display.set_caption('Welcome to Emoji Party')
+
+#Draw a cicle for the head
+pygame.draw.circle( ,  , ,,100)
+
+#Draw two small circles for the eyes
+
+pygame.draw.circle( ,,,,)
+#Draw a smile
+
---
+++ DrawingWithCode/drawings/springFlowers.py
@@ -0,0 +1,13 @@
+#springFlowers.py
+#author: Sarah Daigle
+
+#Draw 3 shapes
+
+#Add A different Color to each Shape
+
+
+#Collect the shapes to form a group or bouquet
+
+#Draw a Stem
+
+
